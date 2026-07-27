92a1d0876cc97a11923bdbd09a7be1d6de0d0c84
mountainallservice <mountainallservice@gmail.com>
2026-07-27 09:12:01 +0000

    fleet: remove both Duster vehicles + their unused copy

--- lib/fleet-data.ts
+++ lib/fleet-data.ts
@@ -3,28 +3,6 @@ export type Car = { name: string; dk: string; img: string; specs: CarSpec[] };
 
 export const cars: Car[] = [
   {
-    name: "Dacia Duster",
-    dk: "fleet.d1",
-    img: "https://d1yei2z3i6k35z.cloudfront.net/16287944/698b8715bd264_2.jpg",
-    specs: [
-      { lk: "spec.Seats", value: "5" },
-      { lk: "spec.Doors", value: "5" },
-      { lk: "spec.Fuel", value: "7.0 l/100km" },
-      { lk: "spec.Gearbox", value: "Manual" },
-    ],
-  },
-  {
-    name: "Duster + Roof Tent",
-    dk: "fleet.d2",
-    img: "https://d1yei2z3i6k35z.cloudfront.net/16287944/698b8715c1bb6_3.jpg",
-    specs: [
-      { lk: "spec.Seats", value: "4" },
-      { lk: "spec.Doors", value: "5" },
-      { lk: "spec.Fuel", value: "7.0 l/100km" },
-      { lk: "spec.Gearbox", value: "Manual" },
-    ],
-  },
-  {
     name: "Renault Master Camper",
     dk: "fleet.d3",
     img: "https://d1yei2z3i6k35z.cloudfront.net/16287944/698b8715bd267_1.jpg",
